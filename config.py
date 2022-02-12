c80f7bbff041b759f7f3826db3b911641d797a5a
senthamizhan96 <43412765+senthamizhan96@users.noreply.github.com>
2022-02-12 16:00:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 class Config(object):
 
-    WARP_ID = os.environ.get('WARP_ID', "None")
-    USE_PROXY = os.environ.get('USE_PROXY', "False")
+    WARP_ID = os.environ.get('WARP_ID', "9b7c402c-6aff-4217-8141-97ebf4ddb904")
+    USE_PROXY = os.environ.get('USE_PROXY', "True")
     PROXY_API = os.environ.get('PROXY_API', "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all")
     # old: https://api.proxyscrape.com?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all
     # new: https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all
